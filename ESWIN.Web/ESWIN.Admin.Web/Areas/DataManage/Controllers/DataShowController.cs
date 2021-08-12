dc04a8b447228b03d6d78aa55f5a12d48510175a
MrBeannnn <328254151@qq.com>
2021-08-12 16:09:14 +0800

    add data area

---
+++ ESWIN.Web/ESWIN.Admin.Web/Areas/DataManage/Controllers/DataShowController.cs
@@ -0,0 +1,19 @@
+﻿using ESWIN.Admin.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESWIN.Admin.Web.Areas.DataManage.Controllers
+{
+    [Area("DataManage")]
+    public class DataShowController : Controller
+    {
+        [AuthorizeFilter("DataManage:Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
